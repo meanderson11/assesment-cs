d6bb00dd03865723300bbb7e28b4271d5d0cea8f
Maria Anderson <Mandersonreka@gmail.com>
2022-05-21 14:43:12 -0600

    finish

--- step2.js
+++ step2.js
@@ -1,14 +1,13 @@
 // 1) sum zero
 function addToZero(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length; j++){
-
-            if (arr[j] === 0){
-                return true;
-            }
-        }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === 0) {
+        return true;
+      }
     }
-    return false
+  }
+  return false;
 }
 
 // let array = [1,2,3,-4, 5, 6, 20];
@@ -19,18 +18,17 @@ function addToZero(arr) {
 
 // 2) Unique Characters
 function hasUniqueChars(str) {
-    for(let i = 0; i < str.length; i++) {
-        let char = str[i];
-        for(j = 0; j < str.length; j++){
-            if(i === j){
-                continue;
-            }
-            else if (char === str[j]){
-                return true;
-            }
-        }
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    for (j = 0; j < str.length; j++) {
+      if (i === j) {
+        continue;
+      } else if (char === str[j]) {
+        return true;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 // console.log(hasUniqueChars("Monday")); true
@@ -41,27 +39,27 @@ function hasUniqueChars(str) {
 
 // 3) Pangram Sentence
 function isPangram(string) {
-    let regex = /([a-z])(?!.*\1)/gi;
-  return (string.match(regex)).length === 26;
+  let regex = /([a-z])(?!.*\1)/gi;
+  return string.match(regex).length === 26;
 }
 
-console.log(isPangram("The quick brown fox jumps over the lazy dog."));// true
-console.log(isPangram("This is not a pangram."));// false
-console.log(isPangram("Pack my box with five dozen liquor jugs."));// true
+console.log(isPangram("The quick brown fox jumps over the lazy dog.")); // true
+console.log(isPangram("This is not a pangram.")); // false
+console.log(isPangram("Pack my box with five dozen liquor jugs.")); // true
 
 // time complexity: O(n)
 // space complexity: O(n)
 
 // 4) Longest Word
 function findLongestWord(arr) {
-    let theLongest = 0
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i].length > theLongest) {
-            theLongest = arr[i].length
-        }
+  let theLongest = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > theLongest) {
+      theLongest = arr[i].length;
     }
-    console.log(theLongest)
-    return theLongest
+  }
+  console.log(theLongest);
+  return theLongest;
 }
 
 findLongestWord(["hi", "hello"]);
